943c64e7c111cd95fa3a13e463ae018447163186
Michał Górny <mgorny@gentoo.org>
2025-04-07 13:47:20 +0200

    Update following variantlib API changes (#2)

--- provider_fictional_tech/plugin.py
+++ provider_fictional_tech/plugin.py
@@ -1,13 +1,10 @@
 from __future__ import annotations
 
-from provider_fictional_tech import __version__
-from variantlib.models.provider import ProviderConfig
 from variantlib.models.provider import VariantFeatureConfig
 
 
 class FictionalTechPlugin:
-    __provider_name__ = "fictional_tech"
-    __version__ = __version__
+    namespace = "fictional_tech"
 
     def _get_supported_technologies(self) -> list[str]:
         """Lookup the system to decide what `technology` are supported on this system.
@@ -25,7 +22,7 @@ class FictionalTechPlugin:
         Returns a list of strings in order of priority."""
         return ["25"]
 
-    def get_supported_configs(self) -> ProviderConfig | None:
+    def get_supported_configs(self) -> list[VariantFeatureConfig]:
         keyconfigs = []
 
         # Top Priority
@@ -40,10 +37,11 @@ class FictionalTechPlugin:
         if (values := self._get_supported_risk_exposure()) is not None:
             keyconfigs.append(VariantFeatureConfig(name="risk_exposure", values=values))
 
-        if keyconfigs:
-            return ProviderConfig(
-                provider=FictionalTechPlugin.__provider_name__,
-                configs=keyconfigs,
-            )
+        return keyconfigs
 
-        return None
+    def get_all_configs(self) -> list[VariantFeatureConfig]:
+        return [
+            VariantFeatureConfig(name="technology", values=["FOAM", "SUPERPOSITION"]),
+            VariantFeatureConfig(name="quantum", values=["auto_chef", "improb_drive"]),
+            VariantFeatureConfig(name="risk_exposure", values=["25", "1000000000"]),
+        ]
